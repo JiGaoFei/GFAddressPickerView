aadcc69893895ae4d1a8c903c8537455e5165bc7
纪高飞 <jigaofei9966@163.com>
2017-05-12 16:54:45 +0800

    添加

---
+++ GFAddressPickerView/GFAddressPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GFAddressPickerView
+//
+//  Created by 纪高飞 on 17/5/12.
+//  Copyright © 2017年 纪高飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
